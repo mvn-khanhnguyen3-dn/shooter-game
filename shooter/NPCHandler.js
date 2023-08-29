92d81cf8bc94505811549d334031c97af19f20d8
nguyenquockhanh11 <nguyenquockhanhkz20@gmail.com>
2023-08-29 11:35:10 +0700

    commit fix loading

--- shooter/NPCHandler.js
+++ shooter/NPCHandler.js
@@ -1,107 +1,107 @@
-import {NPC} from './NPC.js';
-import {GLTFLoader} from '../../libs/three137/GLTFLoader.js';
-import {DRACOLoader} from '../../libs/three137/DRACOLoader.js';
-import {Skeleton, Raycaster, BufferGeometry, Line, Vector3} from '../../libs/three137/three.module.js';
+import { NPC } from './NPC.js';
+import { GLTFLoader } from '../../libs/three137/GLTFLoader.js';
+import { DRACOLoader } from '../../libs/three137/DRACOLoader.js';
+import { Skeleton, Raycaster, BufferGeometry, Line, Vector3 } from '../../libs/three137/three.module.js';
 
-class NPCHandler{
-    constructor( game ){
-        this.game = game;
+class NPCHandler {
+	constructor(game) {
+		this.game = game;
 		this.loadingBar = this.game.loadingBar;
 		this.ready = false;
 		this.load();
 	}
 
-	initMouseHandler(){
+	initMouseHandler() {
 		const raycaster = new Raycaster();
-    	this.game.renderer.domElement.addEventListener( 'click', raycast, false );
-			
-    	const self = this;
-    	const mouse = { x:0, y:0 };
-    	
-    	function raycast(e){
-    		
-			mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-			mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+		this.game.renderer.domElement.addEventListener('click', raycast, false);
+
+		const self = this;
+		const mouse = { x: 0, y: 0 };
+
+		function raycast(e) {
+
+			mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+			mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
 
 			//2. set the picking ray from the camera position and mouse coordinates
-			raycaster.setFromCamera( mouse, self.game.camera );    
+			raycaster.setFromCamera(mouse, self.game.camera);
 
 			//3. compute intersections
-			const intersects = raycaster.intersectObject( self.game.navmesh );
-			
-			if (intersects.length>0){
+			const intersects = raycaster.intersectObject(self.game.navmesh);
+
+			if (intersects.length > 0) {
 				const pt = intersects[0].point;
 				console.log(pt);
 				self.npcs[0].newPath(pt, true);
-			}	
+			}
 		}
-    }
+	}
 
-	reset(){
-		this.npcs.forEach( npc => {
+	reset() {
+		this.npcs.forEach(npc => {
 			npc.reset();
 		})
 	}
 
-    load(){
-        const loader = new GLTFLoader( ).setPath(`${this.game.assetsPath}factory/`);
+	load() {
+		const loader = new GLTFLoader().setPath(`${this.game.assetsPath}factory/`);
 		const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath( '../../libs/three137/draco/' );
-        loader.setDRACOLoader( dracoLoader );
-        this.loadingBar.visible = true;
-		
+		dracoLoader.setDecoderPath('../../libs/three137/draco/');
+		loader.setDRACOLoader(dracoLoader);
+		this.loadingBar.visible = true;
+
 		// Load a GLTF resource
 		loader.load(
 			// resource URL
 			`swat-guy2.glb`,
 			// called when the resource is loaded
 			gltf => {
-				if (this.game.pathfinder){
+				if (this.game.pathfinder) {
 					this.initNPCs(gltf);
-				}else{
+				} else {
 					this.gltf = gltf;
 				}
 			},
 			// called while loading is progressing
 			xhr => {
 
-				this.loadingBar.update( 'swat-guy', xhr.loaded, xhr.total );
+				this.loadingBar.update('swat-guy', xhr.loaded, xhr.total);
 
 			},
 			// called when loading has errors
 			err => {
 
-				console.error( err );
+				console.error(err);
 
 			}
 		);
 	}
-    
-	initNPCs(gltf = this.gltf){
+
+	initNPCs(gltf = this.gltf) {
 		this.waypoints = this.game.waypoints;
-        
+
 		const gltfs = [gltf];
-			
-		for(let i=0; i<3; i++) gltfs.push(this.cloneGLTF(gltf));
+
+		for (let i = 0; i < 3; i++) gltfs.push(this.cloneGLTF(gltf));
 
 		this.npcs = [];
-		
+
 		gltfs.forEach(gltf => {
 			const object = gltf.scene;
 			let rifle, aim;
 
-			object.traverse(function(child){
-				if (child.isMesh){
+			object.traverse(function (child) {
+				if (child.isMesh) {
 					child.castShadow = true;
 					child.frustumCulled = false;
 					if (child.name.includes('Rifle')) rifle = child;
 				}
 			});
 
-			if (rifle){
-				const geometry = new BufferGeometry().setFromPoints( [ new Vector3( 0, 0, 0 ), new Vector3( 1, 0, 0 ) ] );
+			if (rifle) {
+				const geometry = new BufferGeometry().setFromPoints([new Vector3(0, 0, 0), new Vector3(1, 0, 0)]);
 
-				const line = new Line( geometry );
+				const line = new Line(geometry);
 				line.name = 'aim';
 				line.scale.x = 50;
 
@@ -128,44 +128,44 @@ class NPCHandler{
 
 			npc.object.position.copy(this.randomWaypoint);
 			npc.newPath(this.randomWaypoint);
-			
+
 			this.npcs.push(npc);
-			
+
 		});
 
-		this.loadingBar.visible = !this.loadingBar.loaded;
+		this.loadingBar.visible = false;
 		this.ready = true;
 
 		this.game.startRendering();
 	}
 
-	cloneGLTF(gltf){
-	
+	cloneGLTF(gltf) {
+
 		const clone = {
 			animations: gltf.animations,
 			scene: gltf.scene.clone(true)
-		  };
-		
+		};
+
 		const skinnedMeshes = {};
-		
+
 		gltf.scene.traverse(node => {
 			if (node.isSkinnedMesh) {
-			  skinnedMeshes[node.name] = node;
+				skinnedMeshes[node.name] = node;
 			}
 		});
-		
+
 		const cloneBones = {};
 		const cloneSkinnedMeshes = {};
-		
+
 		clone.scene.traverse(node => {
 			if (node.isBone) {
-			  cloneBones[node.name] = node;
+				cloneBones[node.name] = node;
 			}
 			if (node.isSkinnedMesh) {
-			  cloneSkinnedMeshes[node.name] = node;
+				cloneSkinnedMeshes[node.name] = node;
 			}
 		});
-		
+
 		for (let name in skinnedMeshes) {
 			const skinnedMesh = skinnedMeshes[name];
 			const skeleton = skinnedMesh.skeleton;
@@ -179,19 +179,19 @@ class NPCHandler{
 				new Skeleton(orderedCloneBones, skeleton.boneInverses),
 				cloneSkinnedMesh.matrixWorld);
 		}
-		
+
 		return clone;
 
 	}
-    
-    get randomWaypoint(){
-		const index = Math.floor(Math.random()*this.waypoints.length);
+
+	get randomWaypoint() {
+		const index = Math.floor(Math.random() * this.waypoints.length);
 		return this.waypoints[index];
 	}
 
-    update(dt){
-        if (this.npcs) this.npcs.forEach( npc => npc.update(dt) );
-    }
+	update(dt) {
+		if (this.npcs) this.npcs.forEach(npc => npc.update(dt));
+	}
 }
 
 export { NPCHandler };
